de30e05b16557f2d980e5b78e96edb96d65ff13c
Lauren Hershey <laurenzhershey@gmail.com>
2022-04-22 11:52:29 -0700

    added pwd and ls functionality

--- bash.js
+++ bash.js
@@ -2,11 +2,12 @@ process.stdout.write("prompt > ");
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
-    if (cmd === "pwd") {
-      process.stdout.write(`${process.cwd()}`);
-    }
+  if (cmd === "pwd") {
+    require('./pwd.js')();
+  }
+  if (cmd === "ls") {
+    require('./ls.js');
+  }
 
   process.stdout.write(`\nprompt >`)
 })
-
-
